5dac23591398497b707d14848ccb8c5a496ccb41
edit <uic.shivanijaswal@gmail.com>
2023-03-05 23:39:52 +0000

    3

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+sudo npm install -g pm2
+pm2 stop simple_app
+cd SimpleApplication/
+npm install
+pm2 start ./bin/www --name simple_app
